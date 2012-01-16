1b1b2154ff63c1457d86786c502c8014e6a34332
John Mair <jrmair@gmail.com>
2012-01-16 14:53:46 +1300

    migrated all commands to command_class style

--- lib/pry-stack_explorer/commands.rb
+++ lib/pry-stack_explorer/commands.rb
@@ -1,53 +1,114 @@
 require 'pry'
 
 module PryStackExplorer
-  Commands = Pry::CommandSet.new do
-    command "up", "Go up to the caller's context. Accepts optional numeric parameter for how many frames to move up." do |inc_str|
-      inc = inc_str.nil? ? 1 : inc_str.to_i
+  module FrameHelpers
+    private
+    def frame_manager
+      PryStackExplorer.frame_manager(_pry_)
+    end
 
-      if !PryStackExplorer.frame_manager(_pry_)
-        output.puts "Nowhere to go!"
+    def frame_managers
+      PryStackExplorer.frame_managers(_pry_)
+    end
+
+    def prior_context_exists?
+      frame_managers.count > 1 || frame_manager.prior_binding
+    end
+
+    # Return a description of the passed binding object. Accepts an
+    # optional `verbose` parameter.
+    # @param [Binding] b The binding.
+    # @param [Boolean] verbose Whether to generate a verbose description.
+    # @return [String] The description of the binding.
+    def frame_info(b, verbose = false)
+      meth = b.eval('__method__')
+      b_self = b.eval('self')
+      meth_obj = Pry::Method.from_binding(b) if meth
+
+      type = b.frame_type ? "[#{b.frame_type}]".ljust(9) : ""
+      desc = b.frame_description ? "#{b.frame_description}" : "#{PryStackExplorer.frame_manager(_pry_).frame_info_for(b)}"
+      sig = meth_obj ? "<#{signature_with_owner(meth_obj)}>" : ""
+
+      self_clipped = "#{Pry.view_clip(b_self)}"
+      path = "@ #{b.eval('__FILE__')}:#{b.eval('__LINE__')}"
+
+      if !verbose
+        "#{type} #{desc} #{sig}"
       else
-        binding_index = PryStackExplorer.frame_manager(_pry_).binding_index
-        PryStackExplorer.frame_manager(_pry_).change_frame_to binding_index + inc
+        "#{type} #{desc} #{sig}\n      in #{self_clipped} #{path}"
+      end
+    end
+  end
+
+  Commands = Pry::CommandSet.new do
+    command_class "up", "Go up to the caller's context. Accepts optional numeric parameter for how many frames to move up." do
+      include FrameHelpers
+
+      banner <<-BANNER
+        Usage: up [OPTIONS]
+          Go up to the caller's context. Accepts optional numeric parameter for how many frames to move up.
+          e.g: up
+          e.g: up 3
+      BANNER
+
+      def process
+        inc = args.first.nil? ? 1 : args.first.to_i
+
+        if !frame_manager
+          raise Pry::CommandError, "Nowhere to go!"
+        else
+          frame_manager.change_frame_to frame_manager.binding_index + inc
+        end
       end
     end
 
-    command "down", "Go down to the callee's context. Accepts optional numeric parameter for how many frames to move down." do |inc_str|
-      inc = inc_str.nil? ? 1 : inc_str.to_i
+    command_class "down", "Go down to the callee's context. Accepts optional numeric parameter for how many frames to move down." do
+      include FrameHelpers
 
-      if !PryStackExplorer.frame_manager(_pry_)
-        output.puts "Nowhere to go!"
-      else
-        binding_index = PryStackExplorer.frame_manager(_pry_).binding_index
-        if binding_index - inc < 0
-          raise Pry::CommandError, "At bottom of stack, cannot go further!"
+      banner <<-BANNER
+        Usage: down [OPTIONS]
+          Go down to the callee's context. Accepts optional numeric parameter for how many frames to move down.
+          e.g: down
+          e.g: down 3
+      BANNER
+
+      def process
+        inc = args.first.nil? ? 1 : args.first.to_i
+
+        if !frame_manager
+          raise Pry::CommandError, "Nowhere to go!"
         else
-          PryStackExplorer.frame_manager(_pry_).change_frame_to binding_index - inc
+          if frame_manager.binding_index - inc < 0
+            raise Pry::CommandError, "At bottom of stack, cannot go further!"
+          else
+            frame_manager.change_frame_to frame_manager.binding_index - inc
+          end
         end
       end
     end
 
     command_class "show-stack", "Show all frames" do
-      def options(opt)
-        opt.banner unindent <<-USAGE
-            Usage: show-stack [OPTIONS]
-            Show all accessible stack frames.
-            e.g: show-stack -v
-          USAGE
+      include FrameHelpers
 
+      banner <<-BANNER
+        Usage: show-stack [OPTIONS]
+          Show all accessible stack frames.
+          e.g: show-stack -v
+      BANNER
+
+      def options(opt)
         opt.on :v, :verbose, "Include extra information."
       end
 
       def process
-        if !PryStackExplorer.frame_manager(_pry_)
+        if !frame_manager
           output.puts "No caller stack available!"
         else
           content = ""
           content << "\n#{text.bold('Showing all accessible frames in stack:')}\n--\n"
 
-          PryStackExplorer.frame_manager(_pry_).each_with_index do |b, i|
-            if i == PryStackExplorer.frame_manager(_pry_).binding_index
+          frame_manager.each_with_index do |b, i|
+            if i == frame_manager.binding_index
               content << "=> ##{i} #{frame_info(b, opts[:v])}\n"
             else
               content << "   ##{i} #{frame_info(b, opts[:v])}\n"
@@ -57,41 +118,9 @@ module PryStackExplorer
           stagger_output content
         end
       end
-    end
 
-    command "frame", "Switch to a particular frame. Accepts numeric parameter for the target frame to switch to (use with show-stack). Negative frame numbers allowed." do |frame_num|
-      if !PryStackExplorer.frame_manager(_pry_)
-        output.puts "nowhere to go!"
-      else
-        if frame_num
-          PryStackExplorer.frame_manager(_pry_).change_frame_to frame_num.to_i
-        else
-          output.puts "##{PryStackExplorer.frame_manager(_pry_).binding_index} #{frame_info(target, true)}"
-        end
-      end
-    end
-
-    helpers do
-      def frame_info(b, verbose = false)
-        meth = b.eval('__method__')
-        b_self = b.eval('self')
-        meth_obj = Pry::Method.from_binding(b) if meth
-
-        type = b.frame_type ? "[#{b.frame_type}]".ljust(9) : ""
-        desc = b.frame_description ? "#{b.frame_description}" : "#{PryStackExplorer.frame_manager(_pry_).frame_info_for(b)}"
-        sig = meth_obj ? "<#{se_signature_with_owner(meth_obj)}>" : ""
-
-        self_clipped = "#{Pry.view_clip(b_self)}"
-        path = "@ #{b.eval('__FILE__')}:#{b.eval('__LINE__')}"
-
-        if !verbose
-          "#{type} #{desc} #{sig}"
-        else
-          "#{type} #{desc} #{sig}\n      in #{self_clipped} #{path}"
-        end
-      end
-
-      def se_signature_with_owner(meth_obj)
+      private
+      def signature_with_owner(meth_obj)
         if !meth_obj.undefined?
           args = meth_obj.parameters.inject([]) do |arr, (type, name)|
             name ||= (type == :block ? 'block' : "arg#{arr.size + 1}")
@@ -107,9 +136,34 @@ module PryStackExplorer
         else
           "#{meth_obj.name_with_owner}(UNKNOWN) (undefined method)"
         end
-
       end
     end
 
+    # TODO: currently using `arg_string` as a work-around for a Slop
+    # bug where `-2` (negative number) is interpreted as a
+    # non-existent option rather than a non-option
+    command_class "frame", "Switch to a particular frame. Accepts numeric parameter for the target frame to switch to (use with show-stack). Negative frame numbers allowed." do
+      include FrameHelpers
+
+      banner <<-BANNER
+        Usage: frame [OPTIONS]
+          Switch to a particular frame. Accepts numeric parameter for the target frame to switch to (use with show-stack). Negative frame numbers allowed.
+          e.g: frame 4
+          e.g: frame -2
+      BANNER
+
+      def process
+        if !frame_manager
+          raise Pry::CommandError, "nowhere to go!"
+        else
+
+          if !arg_string.empty?
+            frame_manager.change_frame_to arg_string.to_i
+          else
+            output.puts "##{frame_manager.binding_index} #{frame_info(target, true)}"
+          end
+        end
+      end
+    end
   end
 end
